ae60ee308fb1426175db31c0b1e0bb0ecfcef1f3
Maksim-Bakharev <maksimbah@icloud.com>
2018-06-01 17:16:30 +0700

    Function vivoda 2

---
+++ src/OuttextJ.cpp
@@ -0,0 +1,17 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <ctime>
+#include <fstream>
+
+void outtextxyJ(int x, int y, int j)
+{
+     char bif[3];
+     snprintf(bif, sizeof(bif), "%d", j);
+     outtextxy(x, y, bif);
+}
