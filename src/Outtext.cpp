359fabd2aeb82e6b9db71a095d6fe2f7b355f15b
Maksim-Bakharev <maksimbah@icloud.com>
2018-06-01 17:14:43 +0700

    Function vivoda 1

---
+++ src/Outtext.cpp
@@ -0,0 +1,17 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <ctime>
+#include <fstream>
+
+void outtextxy(int x, int y, int lvl)
+{
+     char buf[3];
+     snprintf(buf, sizeof(buf), "%d", lvl);
+     outtextxy(x, y, buf);
+}
