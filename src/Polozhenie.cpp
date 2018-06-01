dec0415002df0af8d10a07f003fe473ea0888bfa
Maksim-Bakharev <maksimbah@icloud.com>
2018-06-01 15:42:17 +0700

    Polozhenie vniz

---
+++ src/Polozhenie.cpp
@@ -0,0 +1,22 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <ctime>
+#include <fstream>
+
+void VNIZ ( int x, int y, int color1,int color2 )
+{
+     setcolor(color1);
+     setfillstyle ( 1, color2 );
+     circle(x,y,19);
+     circle(x-8,y,5);
+     circle(x+8,y,5);
+     moveto(x+5,y+12);
+     lineto(x-5,y+12);
+     floodfill(x,y,color1); 
+}
